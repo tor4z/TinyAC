a58d01140d4ebea5d9d600ef5f9f7f153f74d020
tor4z <vwenjie@hotmail.com>
2022-06-28 12:11:51 +0800

    Fix busybox bugs

--- toolbox/busybox_rootfs/makefiles/setuprootfs.mk
+++ toolbox/busybox_rootfs/makefiles/setuprootfs.mk
@@ -1,9 +1,14 @@
-# $(1) root path
+# $1 root path
 define setup_rootfs
-	@echo "make rootfs at $(1)"
-	@-rm -rf $(1)
-	@mkdir $(1)
+	@echo "make rootfs at $1"
+	@-rm -rf $1
+	@mkdir $1
 
 	@echo "make /bin dir"
-	@mkdir $(1)/bin
+	@mkdir $1/bin
+endef
+
+
+define make_pkg
+	@cd $(ROOTFS_OUTPUT) && tar cf $(TARGET_PKG_NAME) ./*
 endef
